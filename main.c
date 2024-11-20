77690807f5eb64694419776bf14482b729464975
12OUAIS1 <choucharouais@gmail.com>
2024-11-20 19:17:13 +0100

    adding main and correcting video

--- main.c
+++ main.c
@@ -12,19 +12,18 @@ int main() {
     int width, height, durationMovie, durationCredits, fps;
     char u[3];
 
-    // Grayscale Image Input and Output
+
     printf("Enter width and height of the grayscale image: ");
     scanf("%d %d", &width, &height);
     float grayScaleSize = grayScaleImage(width, height);
     printf("Grayscale image size: %.2f bytes\n", grayScaleSize);
 
-    // Colored Bitmap Input and Output
+
     printf("\nEnter width, height, and output unit (bt, ko, mo, go) for the colored image: ");
     scanf("%d %d %s", &width, &height, u);
     float coloredSize = coloredBitmap(width, height, u);
     printf("Colored image size: %.2f %s\n", coloredSize, u);
 
-    // Video Input and Output
     printf("\nEnter width, height, duration of the movie (seconds), duration of the credits (seconds), FPS, and output unit (bt, ko, mo, go): ");
     scanf("%d %d %d %d %d %s", &width, &height, &durationMovie, &durationCredits, &fps, u);
     float videoSize = video(width, height, durationMovie, durationCredits, fps, u);
